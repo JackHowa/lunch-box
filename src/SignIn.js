eba228844f40fbb71b68b7ef06d9b09a46bf7380
jackhowa <jackhowaa@gmail.com>
2017-11-11 12:39:57 -0600

    Add sign in auth info from google

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import { auth, googleAuthProvider } from './firebase';
+import React, { Component } from "react";
+
+// importing bare pieces for auth
+import { auth, googleAuthProvider } from "./firebase";
 
 class SignIn extends Component {
+
   render() {
     return (
       <div className="SignIn">
+        <button onClick={() => auth.signInWithPopup(googleAuthProvider)}>
+          Sign In
+        </button>
       </div>
     );
   }
