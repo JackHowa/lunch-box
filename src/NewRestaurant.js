c8f8c79249ea0a29b6fc53de61508f846fc270b8
jackhowa <jackhowaa@gmail.com>
2017-12-05 12:12:39 -0600

    Add comments

--- src/NewRestaurant.js
+++ src/NewRestaurant.js
@@ -1,6 +1,11 @@
-import React, { Component, PropTypes } from "react";
+import React, {
+  Component,
+  PropTypes
+} from "react";
 import "./NewRestaurant.css";
-import { database } from "./firebase";
+import {
+  database
+} from "./firebase";
 
 class NewRestaurant extends Component {
   constructor() {
@@ -15,24 +20,39 @@ class NewRestaurant extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.restaurantsRef.push({ name: this.state.name });
+    // will push a new restaurant onto that node 
+    this.restaurantsRef.push({
+      name: this.state.name
+    });
   }
 
   render() {
-    const { name } = this.state;
+    const {
+      name
+    } = this.state;
 
-    return (
-      <form className="NewRestaurant">
-        <input
-          type="text"
-          value={name}
-          placeholder="Name of Fine Establishment"
-          onChange={event => this.setState({ name: event.target.value })}
-        />
-        <button onClick={this.handleSubmit} disabled={!name}>
-          Submit
-        </button>
-      </form>
+    return ( <
+      form className = "NewRestaurant" >
+      <
+      input type = "text"
+      value = {
+        name
+      }
+      placeholder = "Name of Fine Establishment"
+      onChange = {
+        event => this.setState({
+          name: event.target.value
+        })
+      }
+      /> <
+      button onClick = {
+        this.handleSubmit
+      }
+      disabled = {!name
+      } >
+      Submit <
+      /button> <
+      /form>
     );
   }
 }
